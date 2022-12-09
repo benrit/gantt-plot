a18419fb9ea8b2f8b0394276b94daa3b37840fda
benrit <ritterbwa@gmail.com>
2022-12-09 11:21:59 -0500

    adding tree features. flatten tree to a flat array. adding createNode for tree structure.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ function App() {
 
     const {treeData, gnattData} = useGnattPlot()
     return (
-        <div className="App">
-            <svg>
+        <div className="App" style={{display: "flex", height: 500, overflowY: "auto"}}>
+            <svg height={800} style={{flex: "none", width: 300, backgroundColor: "lightgray"}}>
                 {treeData}
             </svg>
-            <svg>
+            <svg height={800} style={{flex: "auto", backgroundColor: "rgba(0,0,0, 0.2)"}}>
                 {gnattData}
             </svg>
         </div>
--- src/components/GnattPlot.tsx
+++ src/components/GnattPlot.tsx
@@ -1,41 +1,101 @@
 import React, {useEffect, useState} from "react";
 
 
-interface TreeNodeSVG_i{
+interface TreeNodeSVG_i {
     x: number;
     y: number;
     text: string
 }
 
 
+interface GnattNodeSVG_i {
+    x: number;
+    y: number;
+    text: string
+}
 
-const GnattNodeSVG = () => {
 
+interface TreeNode_i {
+    name: string;
+    parent: TreeNode_i;
+    isOpen: boolean;
+    isEditing: boolean;
+    hasChildren: boolean;
+    children: TreeNode_i[]
+}
+
+const GnattNodeSVG = ({x, y, text}: GnattNodeSVG_i) => {
+
+    return (
+        <g transform={`translate(${x} ${y})`}>
+            <text dominantBaseline="hanging">{text}</text>
+        </g>
+    )
 }
 
 
 const TreeNodeSVG = ({x, y, text}: TreeNodeSVG_i) => {
 
-    return(
+    return (
         <g transform={`translate(${x} ${y})`}>
-            <text>{text}</text>
+            <text dominantBaseline="hanging">{text}</text>
         </g>
     )
 }
 
-export default function useGnattPlot(){
-    const [entries, setEntries] = useState<{treeData: JSX.Element[], gnattData: JSX.Element[]}>({gnattData: [], treeData:[]})
+const createNode = (name: string, parent: object | null) => {
 
-    useEffect(()=>{
-        const temp:JSX.Element[] = []
+    return {
+        name: name,
+        parent: parent,
+        isOpen: true,
+        isEditing: false,
+        hasChildren: false,
+        children: []
+    };
+}
+
+const flattenTree = (tree: TreeNode_i): TreeNode_i[] => {
+
+    const temp: TreeNode_i[] = []
+
+    const iter = (i: TreeNode_i) => {
+        const hasChildren = i.children && i.children.length > 0;
+        temp.push(i);
+
+        if (hasChildren) {
+            i.children.forEach((n) => {
+                iter(n);
+            })
+        }
+    }
+
+    tree.children.forEach((item) => {
+        iter(item)
+    })
+
+    return temp;
+}
+
+export default function useGnattPlot() {
+    const [entries, setEntries] = useState<{ treeData: JSX.Element[], gnattData: JSX.Element[] }>({
+        gnattData: [],
+        treeData: []
+    })
+
+    useEffect(() => {
+        const tempTree: JSX.Element[] = []
+        const tempGnatt: JSX.Element[] = []
+
         for (let i = 0; i < 10; i++) {
-                temp.push( TreeNodeSVG({x: 10, y:(i*25)+10, text: "test"}))
-            }
+            tempTree.push(TreeNodeSVG({x: 10, y: (i * 25) + 10, text: "Tree"}))
+            tempGnatt.push(GnattNodeSVG({x: 10, y: (i * 25) + 10, text: "Gnatt"}))
+        }
 
-        setEntries((prevState)=>{
-            return {treeData: temp, gnattData: temp}
+        setEntries(() => {
+            return {treeData: tempTree, gnattData: tempGnatt}
         });
-        }, [])
+    }, [])
 
 
     return entries
